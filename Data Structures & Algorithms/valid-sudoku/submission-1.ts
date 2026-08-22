613a8269d24ab28b2c9451272c6d506702935bdd
Arun Thiyaagarajan <76865725+Arun-Thiyaagarajan@users.noreply.github.com>
2026-08-22 10:40:20 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.ts
@@ -0,0 +1,78 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const numSet = new Set<string>();
+        const n = board.length;
+        // const flattenBoard = board.flat();
+
+        for (let i = 0; i < n; i++) {
+            const row = board[i];
+            for (let j = 0; j < n; j++) {
+                const num = row[j];
+                // Checks for the duplicate in the horizontal rows, if has returns false.
+                if (num != "." && numSet.has(num)) {
+                    return false;
+                }
+                // else, add to rowSet
+                numSet.add(num);
+            }
+
+            // clear the set
+            numSet.clear();
+        }
+
+
+        for (let i = 0; i < n; i++) {
+            const column = board.map(row => row[i]);
+            for (let j = 0; j < n; j++) {
+                const num = column[j];
+                // Checks for the duplicate in the horizontal rows, if has returns false.
+                if (num != "." && numSet.has(num)) {
+                    return false;
+                }
+                // else, add to rowSet
+                numSet.add(num);
+            }
+
+            // clear
+            numSet.clear();
+        }
+
+        for (let r = 0; r < 9; r += 3) {
+            for (let c = 0; c < 9; c += 3) {
+                if (this.hasDuplicateBox(board, r, c)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public hasDuplicateBox(
+        grid: string[][],
+        startRow: number,
+        startCol: number
+    ): boolean {
+        const seen = new Set<string>();
+
+        for (let r = startRow; r < startRow + 3; r++) {
+            for (let c = startCol; c < startCol + 3; c++) {
+            const value = grid[r][c];
+
+            if (value === ".") continue;
+
+            if (seen.has(value)) {
+                return true;
+            }
+
+            seen.add(value);
+        }
+    }
+
+        return false;
+    }
+}
