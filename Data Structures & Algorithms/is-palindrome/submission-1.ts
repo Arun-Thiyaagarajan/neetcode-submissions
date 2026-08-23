b35494afce4a76d5858f51aa1adfae7c735cf85b
Arun Thiyaagarajan <76865725+Arun-Thiyaagarajan@users.noreply.github.com>
2026-08-23 10:35:49 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        const sArr = s.split("");
+        const reversed = sArr.reverse().join("");
+        console.log(s, reversed)
+        return reversed === s;
+    }
+}
